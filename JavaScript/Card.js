75fcf62fe5d78af97338711814723a44b6104fea
Christopher Scully <scullycphone@gmail.com>
2019-11-12 22:12:04 -0600

    Log In and Sign In forms

--- JavaScript/Card.js
+++ JavaScript/Card.js
@@ -3,12 +3,10 @@
 $(document).ready(function(){
     $(".hvr").hover(
         function(){
-        console.log("add");
         $(this).removeClass("col-lg-3");
         $(this).addClass("col-lg-4");
     },
         function(){
-        console.log("remove");
         $(this).removeClass("col-lg-4");
         $(this).addClass("col-lg-3");
     });
